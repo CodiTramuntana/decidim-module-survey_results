5f13040ae24b77c73493b9247dec7802c77b544a
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-03-01 19:06:21 +0100

    REFACTOR: Move methods to where they belong

--- app/helpers/decidim/survey_results/survey_results_helper.rb
+++ app/helpers/decidim/survey_results/survey_results_helper.rb
@@ -9,13 +9,6 @@ module Decidim
         render partial: results.partial_name, locals: {full_questionnaire:, results:}
       end
 
-      def total_anwers_files_answered_percentage(question)
-        "#{(total_answers_files_count(question)[:with_attachments] * 100)/total_participants}%"
-      end
-
-      def total_anwers_files_not_answered_percentage(question)
-        "#{(total_answers_files_count(question)[:without_attachments] * 100)/total_participants}%"
-      end
     end
   end
 end
--- app/models/decidim/survey_results/files_question_results.rb
+++ app/models/decidim/survey_results/files_question_results.rb
@@ -7,6 +7,15 @@ module Decidim
       def initialize(full_questionnaire, question)
         super(full_questionnaire, question, "files_question_results")
       end
+
+      def total_anwers_files_answered_percentage(question)
+        "#{(total_answers_files_count(question)[:with_attachments] * 100)/total_participants}%"
+      end
+
+      def total_anwers_files_not_answered_percentage(question)
+        "#{(total_answers_files_count(question)[:without_attachments] * 100)/total_participants}%"
+      end
+
     end
   end
 end
