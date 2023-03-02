c606be8824e3ae7c1b537f90335abdf13b98dd55
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-03-02 08:38:06 +0100

    WIP|REFACTOR matrix results

--- app/models/decidim/survey_results/matrix_question_results.rb
+++ app/models/decidim/survey_results/matrix_question_results.rb
@@ -8,25 +8,27 @@ module Decidim
         super(full_questionnaire, question, "matrix_question_results")
       end
 
+      # question.answer_options -> columns
+      # question.matrix_rows -> rows
+      # answers -> user responses
+      # Answer has many choices (AnswerChoices)
       def multiple_chart_question
-        datasets = []
-
         labels = question.matrix_rows.map{ |row| translated_attribute(row.body) }
 
-        question.answer_options.map do |answer_option|
-          data_row = []
+        data_rows = []
+        question.answer_options.each do |answer_option|
           full_questionnaire.answers.where(question: question).each do |answer|
             question.matrix_rows.each do |matrix_row|
 
-              row_choices = answer.question.answer_options.map do |answer_option|
+              row_choices = question.answer_options.map do |answer_option|
                 answer.choices.where(matrix_row: matrix_row, answer_option: answer_option).count
               end
-              data_row << row_choices
+              data_rows << row_choices
             end
           end
         end
 
-        { labels: labels, data: datasets }
+        { labels: labels, data: data_rows }
       end
     end
   end
