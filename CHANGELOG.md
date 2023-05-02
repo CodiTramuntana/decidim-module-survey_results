91e225338b3a731937f358a5a3b02a3d1b13db4e
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-05-02 22:52:59 +0200

    FIX: Correct path in buton to browse to the results

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+= Changelog
+
+== 0.0.1 BETA/MINOR (02/05/2023)
+
+- Release beta
--- app/overrides/decidim/forms/questionnaires/questionnaire_show.rb
+++ app/overrides/decidim/forms/questionnaires/questionnaire_show.rb
@@ -5,5 +5,5 @@ Deface::Override.new(virtual_path: "decidim/forms/questionnaires/show",
   original: "f2fe5ee7066f632fb4a930c922c680ddfbdc1520",
   insert_after: "div.card",
   text: "
-    <%= link_to 'Ver los resultados', decidim_survey_results.survey_results_path(component_id: current_component.id) %>
+    <%= link_to I18n.t('survey_results.questionnaire_show.see_results'), decidim_survey_results.survey_results_path(component_id: current_component.id) %>
   ")
